ab266104561a4a1dc36ae0603c15f687365f4cd5
rbillings2022 <rbillings2022@fau.edu>
2026-02-17 10:37:38 -0500

    added comments and fix placeholder graph

--- cytoscape.js
+++ cytoscape.js
@@ -4,19 +4,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const cy = cytoscape({
         container: document.getElementById('cytoscape'),
 
+        //Cytoscape Syntax to style graph
         elements: [
-            { data: { id: 'PC1', type: 'pc'} },
+            //Making connections with edges and nodes
+            { data: { id: 'PC1', type: 'pc'} },// Adding elements: id: 'name', type: 'name'
             { data: { id: 'Switch', type: 'switch' } },
-            { data: { id: 'Router1' } },
-            { data: { id: 'Router2' } },
-            { data: { id: 'link1', source: 'PC1', target: 'Router1' } },
+            { data: { id: 'Router1', type: 'router' } },
+            { data: { id: 'Router2', type: 'router' } },
             { data: { id: 'link2', source: 'Router2', target: 'Router1' } },
-            { data: { id: 'link3', source: 'Switch', target: 'PC1' } }
+            { data: { id: 'link3', source: 'Switch', target: 'PC1' } },
+            { data: { id: 'link4', source: 'Router1', target: 'Switch' } }
         ],
 
+        //Style elements in graph
         style: [
             {
-                selector: 'node',
+                selector: 'node[type="pc"]',
                 style: {
                     'background-image': 'icons/computer.png',
                     'background-color': '#172738',
@@ -31,8 +34,30 @@ document.addEventListener("DOMContentLoaded", function () {
                     'width': 3,
                     'line-color': '#ccc'
                 }
-                
-
+            },
+            {
+                selector: 'node[type="switch"]',
+                style: {
+                    'background-image': 'icons/switch.png',
+                    'background-color': '#172738',
+                    'label': 'data(id)',
+                    'color': '#fff',
+                    'text-halign': 'center',
+                    'width': 60,
+                    'height': 60
+                }
+            },
+            {
+                selector: 'node[type="router"]',
+                style: {
+                    'background-image': 'icons/router.png',
+                    'background-color': '#172738',
+                    'label': 'data(id)',
+                    'color': '#fff',
+                    'text-halign': 'left',
+                    'width': 40,
+                    'height': 40
+                }
             },
 
         ],
@@ -40,6 +65,8 @@ document.addEventListener("DOMContentLoaded", function () {
             name: 'circle'
         }
     });
+    
+    //Graph behavior. If you tap-click node PC1 it removes that node.
     cy.on('tap', 'node[type="pc"]', function(evt) {
         console.log(evt.target.id());
         cy.$('#PC1').remove();
